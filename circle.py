8bcf892b4e350089c99b52b677874c8656178e78
Lshob <Lshobuhova@bk.ru>
2025-10-11 04:07:10 +0300

    Добавил комментарии в файл circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает число r, возвращает pi*r^2 (площадь круга)'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает число r, возвращает 2*pi*r (периметр круга)'''
     return 2 * math.pi * r
 
